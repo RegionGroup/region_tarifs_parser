3f0b61f804ccc398183654c5ab50394623773f87
Danila Kizanenko <ki.alkiviad@gmail.com>
2019-12-06 11:08:43 +0200

    vegafix

--- app/parser/od/vega.py
+++ app/parser/od/vega.py
@@ -1,18 +1,20 @@
 from parser.parser import Parser
 from parser.tools import clear_text
 
+
 def trf_parser(**params):
 
     parser = Parser(
-        name='Vega',
-        url='https://vega.ua/rus/for_home/superconnect_fttb',
-        habitation=params['habitation'])
+        name="Vega",
+        url="https://vega.ua/rus/for_home/superconnect_fttb",
+        habitation=params["habitation"],
+    )
 
     tarif_dic = {}
     all_tarifs = []
 
     soup = parser.parse()
-    main_div = soup.find('div' ,attrs='blockID556').text.strip()
-    all_tarifs = [{'title':main_div}]
+    main_div = soup.find("div", attrs="blockID1201").text.strip()
+    all_tarifs = [{"title": main_div}]
     parser.set_tarifs(all_tarifs)
     return parser
